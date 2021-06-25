aba00830fc38c8edba044e45829a115e871badbc
SebastianMacaya <contacto@sebastianmacaya.com>
2021-06-25 12:02:42 -0300

    se agrega brand carousel responsive

---
+++ brandcarousel.js
@@ -0,0 +1,18 @@
+$(function () {
+  $("#carousel-multiple").on("slide.bs.carousel", function (e) {
+    var itemsPerSlide = parseInt($(this).attr("data-maximum-items-per-slide")),
+      totalItems = $(".carousel-item", this).length,
+      reserve = 1, //do not change
+      $itemsContainer = $(".carousel-inner", this),
+      it = itemsPerSlide + reserve - (totalItems - e.to);
+    console.log($itemsContainer);
+    if (it > 0) {
+      for (var i = 0; i < it; i++) {
+        $(".carousel-item", this)
+          .eq(e.direction == "left" ? i : 0)
+          // append slides to the end/beginning
+          .appendTo($itemsContainer);
+      }
+    }
+  });
+});
