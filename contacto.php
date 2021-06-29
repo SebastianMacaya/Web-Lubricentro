ce0ae5ca370413b96bc4f80862f735cfc2dd208c
SebastianMacaya <contacto@sebastianmacaya.com>
2021-06-29 13:55:41 -0300

    se hace funcional el formulario con php

---
+++ contacto.php
@@ -0,0 +1,16 @@
+<?php
+$email_to = "sebastian@lubricentroelcacique.com.ar";
+$email_subject = "Contacto desde el sitio web";
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+$email_message .= "E-mail: " . $_POST['mail'] . "\n";
+$email_message .= "Telefono: " . $_POST['telefono'] . "\n";
+$email_message .= "Asunto: " . $_POST['asunto'] . "\n";
+$email_message .= "Mensaje: " . $_POST['mensaje'] . "\n\n";
+$email_message .= "NewsLetter: " . $_POST['newsletter'] . "\n\n";
+$headers = 'From: ' . $email_to . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+echo '<script type="text/javascript">
+		    	alert("Gracias por contactarte con Lubricentro el Cacique, nos comunicaremos a la brevedad");
+		    	window.location.href="index.html";
+		      </script>';	  
